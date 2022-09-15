e5975f9879379961ef5349f2cf19cc39eff6601a
erebus-01 <phamhiep.it.epu@gmail.com>
2022-09-15 22:42:34 +0700

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
-const YAML = require('js-yaml');
 const express = require('express');
 const multer = require('multer');
+const path = require('path');
 
 const app = express();
 const PORT = 7000;
@@ -10,39 +10,20 @@ const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "upload");
   },
-  file: (req, file, cb) => {
-    cb(null, file.originalname + "_" + Date.now());
-  },      
-  fileFilter(req, file, cb) {
-    if (!file.originalname.match(/\.(png|jpg)$/)) { 
-      return cb(new Error('Please upload a Image'))
-    }
-    cb(undefined, true)
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
   }
 })
 
 const upload = multer({storage: storage});
 
 app.post("/upload", upload.single("data"), (req, res) => {
-  // if(req.file) {
-  //   try{
-  //     const raw = fs.readFileSync(`upload/${req.file.filename}`);
-  //     console.log(raw);
-  //   }
-  //   catch(err) {  
-  //     console.log(err);
-  //     res.status(500).send({
-  //       ok: false,
-  //       err: "Dang bi loi roi nhe!!!"
-  //     })
-  //   }
-  // }
-  // else{
-  //   res.status(400).send({
-  //     ok: true,
-  //     err: "Da chon file upload dau ???"
-  //   })
-  // }\
+  if(!req.file) {
+    res.status(500).send({
+      ok: false,
+      err: "Da chon file upload dau ???"
+    })
+  }
   res.send(req.file)
 })
 
